509c05c05bfc2f1008e8e61f5493ca6dfd04f004
Asi Givati <asilavista1@walla.co.il>
2024-11-21 21:07:40 +0200

    LocalAuthenticationHandler reason

--- VisaInterviewTest/Handlers/LocalAuthenticationHandler.swift
+++ VisaInterviewTest/Handlers/LocalAuthenticationHandler.swift
@@ -8,14 +8,13 @@
 import LocalAuthentication
 
 class LocalAuthenticationHandler {
-    func authenticate() async -> (success:Bool, error:Error?) {
+    func authenticate(reason:String = "We need to unlock your data.") async -> (success:Bool, error:Error?) {
         let context = LAContext()
         var error: NSError?
         guard context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
             return (false, NSError(domain: "No biometrics found", code: -1, userInfo: nil))
         }
         
-        let reason = "We need to unlock your data."
         return await withCheckedContinuation { continuation in
             context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
                 return continuation.resume(returning: (success, authenticationError))
